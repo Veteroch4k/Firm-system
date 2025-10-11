a7c751a3ac95177340695fa710ee204b418c07cd
Veteroch4k <indefinite05@mail.ru>
2025-10-11 18:59:01 +0700

    Transfered several classes to factory-service and also implemented db migrations

--- services/backend/warehouse-service/src/main/java/com/veteroch4k/warehouse/models/Material.java
+++ services/backend/warehouse-service/src/main/java/com/veteroch4k/warehouse/models/Material.java
@@ -1,10 +1,15 @@
 package com.veteroch4k.warehouse.models;
 
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,15 +18,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @Entity
-@Table(name = "materials")
+@Table(name = "material")
 public class Material {
 
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
 
   private String name;
-
 
 
 }
--- services/backend/warehouse-service/src/main/java/com/veteroch4k/warehouse/models/MaterialAccounting.java
+++ services/backend/warehouse-service/src/main/java/com/veteroch4k/warehouse/models/MaterialAccounting.java
@@ -1,38 +1,57 @@
 package com.veteroch4k.warehouse.models;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.util.Assert;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
-@Table(name = "materials_accounting")
+@Table(name = "material_accounting")
 public class MaterialAccounting {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
 
-  private int material_id;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "material_id")
+  private Material material;
 
   private int quantity;
 
-  private int product_id;
+  @Enumerated(EnumType.STRING)
+  private MovementType type;
 
-  private int employer_id;
+  @Column(name = "factory_id", nullable = false)
+  private Long factoryId;
+
+
+  @Column(name = "employer_id", nullable = false)
+  private Long employerId;
+
 
   private LocalDate date;
 
-  private int order_id;
+}
 
-
+enum MovementType {
+  INCOME,    // Приход на склад
+  OUTCOME,   // Расход со склада
+  TRANSFER   // Перемещение между складами
 }
