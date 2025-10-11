276ca226a9adc0317a9054cadebb82442f1483cc
Veteroch4k <indefinite05@mail.ru>
2025-10-11 16:34:23 +0700

    Added factorty_materials table

---
+++ services/backend/factory-service/src/main/java/com/veteroch4k/factory_service/models/FactoryMaterials.java
@@ -0,0 +1,43 @@
+package com.veteroch4k.factory_service.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Fetch;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "factory_materials")
+public class FactoryMaterials {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "factory_id")
+  private Factory factoryId;
+
+  @Column(name = "material_id", nullable = false)
+  private Long materialId;
+
+  private int quantity;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+
+}
