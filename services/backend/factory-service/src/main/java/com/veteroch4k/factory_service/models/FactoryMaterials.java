f98f8d95834154aac7df6902c1274c9adb79393c
Veteroch4k <indefinite05@mail.ru>
2025-10-11 16:44:33 +0700

    minor fix

--- services/backend/factory-service/src/main/java/com/veteroch4k/factory_service/models/FactoryMaterials.java
+++ services/backend/factory-service/src/main/java/com/veteroch4k/factory_service/models/FactoryMaterials.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -32,12 +33,11 @@ public class FactoryMaterials {
   private Factory factoryId;
 
   @Column(name = "material_id", nullable = false)
-  private Long materialId;
+  private int materialId;
 
   private int quantity;
 
-  @CreationTimestamp
-  private LocalDateTime createdAt;
+  private LocalDate createdAt;
 
 
 }
