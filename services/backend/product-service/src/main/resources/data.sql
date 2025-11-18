38fbecf3f580b68a731901e952340b0664d89ff6
Veteroch4k <indefinite05@mail.ru>
2025-11-18 19:01:40 +0700

    fit: add sql initialization data.sql file

---
+++ services/backend/product-service/src/main/resources/data.sql
@@ -0,0 +1,17 @@
+
+INSERT INTO product_service.product (id, description) VALUES
+(0, 'каркас для седана'),
+(1, 'каркас для пикапа'),
+(2, 'каркас для кабриолета'),
+(3, 'сиденья'),
+(4, 'руль'),
+(5, 'приборная панель'),
+(6, 'двигатель'),
+(7, 'трансмиссия'),
+(8, 'электрооборудование');
+
+-- drawings.sql
+INSERT INTO product_service.drawing (id, op_id, factory_id) VALUES
+(0, 0, 0),
+(1, 1, 1),
+(2, 2, 2);
