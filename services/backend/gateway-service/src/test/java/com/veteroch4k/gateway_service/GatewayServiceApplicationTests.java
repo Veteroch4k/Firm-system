02da5cf1fa11a03a027c4f55b7d46701011e80fb
Veteroch4k <indefinite05@mail.ru>
2026-01-02 17:20:58 +0700

    feat: add routing-service

---
+++ services/backend/gateway-service/src/test/java/com/veteroch4k/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veteroch4k.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
