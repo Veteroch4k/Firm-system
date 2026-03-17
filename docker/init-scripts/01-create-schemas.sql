abcb0a007d6df06249eac318778776c8423a32ed
Veteroch4k <indefinite05@mail.ru>
2026-03-18 04:17:24 +0700

    feat: add kafka properties

---
+++ docker/init-scripts/01-create-schemas.sql
@@ -0,0 +1,6 @@
+CREATE SCHEMA IF NOT EXISTS order_service;
+CREATE SCHEMA IF NOT EXISTS warehouse_service;
+CREATE SCHEMA IF NOT EXISTS toolwarehouse_service;
+CREATE SCHEMA IF NOT EXISTS employer_service;
+CREATE SCHEMA IF NOT EXISTS product_service;
+CREATE SCHEMA IF NOT EXISTS factory_service;
