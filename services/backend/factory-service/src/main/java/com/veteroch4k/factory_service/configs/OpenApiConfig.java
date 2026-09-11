a56675d9812efe2e326d8e98331fd13f1e772b82
Veteroch4k <viktor.popov2005@mail.ru>
2026-09-12 04:25:19 +0700

    feat: implement observing (metrics-traces-logs)

--- services/backend/factory-service/src/main/java/com/veteroch4k/factory_service/configs/OpenApiConfig.java
+++ services/backend/factory-service/src/main/java/com/veteroch4k/factory_service/configs/OpenApiConfig.java
@@ -3,6 +3,12 @@ package com.veteroch4k.factory_service.configs;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -16,4 +22,13 @@ import io.swagger.v3.oas.annotations.info.Info;
                 )
         )
 )
-public class OpenApiConfig {}
+@Component
+public class OpenApiConfig {
+
+    @Bean
+    public OpenApiCustomizer serverUrlCustomizer() {
+        return openApi -> openApi.setServers(
+                List.of(new Server().url("/factory").description("via gateway"))
+        );
+    }
+}
