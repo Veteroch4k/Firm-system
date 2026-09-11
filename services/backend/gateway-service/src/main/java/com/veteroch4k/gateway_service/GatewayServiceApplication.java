d1df5d9b3c92d505751b1550c692c7240ff9edf2
Veteroch4k <viktor.popov2005@mail.ru>
2026-09-12 04:28:31 +0700

    feat: implement observing (metrics-traces-logs)

--- services/backend/gateway-service/src/main/java/com/veteroch4k/gateway_service/GatewayServiceApplication.java
+++ services/backend/gateway-service/src/main/java/com/veteroch4k/gateway_service/GatewayServiceApplication.java
@@ -3,6 +3,7 @@ package com.veteroch4k.gateway_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class GatewayServiceApplication {
 
--- services/backend/gateway-service/src/main/java/com/veteroch4k/gateway_service/SecurityConfig.java
+++ services/backend/gateway-service/src/main/java/com/veteroch4k/gateway_service/SecurityConfig.java
@@ -17,6 +17,7 @@ public class SecurityConfig {
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchanges -> exchanges
                         .pathMatchers("/actuator/**").permitAll()
+                        .pathMatchers("/*/v3/api-docs/**", "/v3/api-docs/**").permitAll()
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
