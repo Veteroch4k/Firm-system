bef098d9aa27b7979ceccb25265b52659f4bde94
Veteroch4k <viktor.popov2005@mail.ru>
2026-09-12 18:22:54 +0700

    fix: fix maven tests

--- services/backend/gateway-service/src/test/java/com/veteroch4k/gateway_service/GatewayServiceApplicationTests.java
+++ services/backend/gateway-service/src/test/java/com/veteroch4k/gateway_service/GatewayServiceApplicationTests.java
@@ -2,9 +2,18 @@ package com.veteroch4k.gateway_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class GatewayServiceApplicationTests {
+
+	@MockitoBean
+	private ReactiveClientRegistrationRepository clientRegistrationRepository;
+
+	@MockitoBean
+	private ReactiveJwtDecoder reactiveJwtDecoder;
 
 	@Test
 	void contextLoads() {
