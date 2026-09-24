ea4987b77888ad82653e7fda0473460fff9a55b6
Victor <104749363+Veteroch4k@users.noreply.github.com>
2026-09-24 15:10:56 +0700

    feat: implement 2 different profiles: dev and prod

--- services/backend/common-lib/src/main/java/com/veteroch4k/firm/common/configs/SecurityConfig.java
+++ services/backend/common-lib/src/main/java/com/veteroch4k/firm/common/configs/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.veteroch4k.firm.common.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,10 +18,17 @@ import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
 public class SecurityConfig {
 
+    @Configuration
+    @Profile("!dev")
+    @EnableMethodSecurity
+    public static class MethodSecurityConfig {
+    }
+
+
     @Bean
+    @Profile("!dev")
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
@@ -49,6 +57,17 @@ public class SecurityConfig {
                     .collect(Collectors.toList());
         });
         return converter;
+    }
+
+    @Bean
+    @Profile("dev")
+    public SecurityFilterChain devSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+        return http.build();
     }
 
 
